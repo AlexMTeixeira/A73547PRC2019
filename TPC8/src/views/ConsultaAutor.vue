5cd022305857fffe6491922df4093d41e9dcef85
anguinho <anguinho95@gmail.com>
2019-05-13 10:26:26 +0100

    TPC8 final

---
+++ TPC8/src/views/ConsultaAutor.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+      <AtorFilmes :idAtor="$route.params.id"/>
+      <v-layout>
+          <v-btn color="info" @click="$router.go(-1)">
+              Voltar à página anterior
+          </v-btn>
+      </v-layout>
+  </v-container>
+</template>
+
+<script>
+import AtorFilmes from '@/components/AtorFilmes'
+
+export default {
+  components: {
+    AtorFilmes
+  }
+}
+</script>
